4a316608b344444395b490e9d85cf5dac1242871
kdhn97 <ehdgus3726@naver.com>
2024-09-09 17:37:37 +0900

    0909

---
+++ yt-music-clone-next/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Clone YT Music",
+  description: "Clone Youtube Music",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
